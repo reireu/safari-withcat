fb213c411000712547f8a8b5063af4439f565b40
玲 <136562726+rena-tech@users.noreply.github.com>
2024-06-04 23:29:03 +0900

    すべてのjs埋め

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
-// server.js
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-// サンプルルート
+// 静的ファイルの提供
+app.use(express.static(path.join(__dirname, '../public')));
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-// サーバーの起動
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
